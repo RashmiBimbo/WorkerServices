ee03f4f32844fa56705a02e5eb3a7a56c5827e8c
RASHMI GUPTA <rashmi.gupta@grupobimbo.com>
2024-09-29 20:38:39 +0530

    Initial Commit for angular app

---
+++ EntitiesAngularApp/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { EntitiesComponent } from './entities/entities.component';
+
+export const routes: Routes = [
+  {
+    path: "entities", component: EntitiesComponent
+  }
+];
---
+++ EntitiesAngularApp/src/app/entities/entities.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entities',
+  standalone: true,
+  imports: [],
+  templateUrl: './entities.component.html',
+  styleUrl: './entities.component.scss'
+})
+export class EntitiesComponent {
+
+}
